13cc19f922bb05d595d3245518832da2a2263015
Diego <diegoiretamalp@gmail.com>
2022-04-25 23:34:25 -0400

    Initial commit

---
+++ templates/landing/vista-footer/footer.php
@@ -0,0 +1,86 @@
+<!-- Footer -->
+<footer class="footer">
+  <!-- Section: Social media -->
+    <section
+    class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+  >
+    <!-- Left -->
+    
+    <!-- Left -->
+
+
+    
+    </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+        <div class="container text-center text-md-start mt-5">
+        <!-- Grid row -->
+            <div class="row mt-3">
+                
+                
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4 text-footer-left" >
+                <!-- Content -->
+                    <img class="logo-imagen-duoc-footer  " src="../public/assets/img/duoc.jpg" alt="logo-tipo2" />
+                    <p>
+                        Instituto Profesional Duoc UC
+                    </p>
+                    <p>
+                        Acreditado
+                    </p>
+                    <p>
+                        7 años desde agosto 2017 hasta agosto 2024
+                    </p>
+                    <p>
+                        Docencia de Pregrado
+                    </p>  
+                    <p>
+                        Gestión Institucional
+                    </p>
+                    <p>
+                        Vinculación con el Medio
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 text-footer-rigth ">
+                <!-- Links -->
+                <h6 class="text-uppercase fw-bold mb-4">
+                    Contact
+                </h6>
+                <p>
+                        Instituto Profesional Duoc UC
+                    </p>
+                    <p>
+                        Acreditado
+                    </p>
+                    <p>
+                        7 años desde agosto 2017 hasta agosto 2024
+                    </p>
+                    <p>
+                        Docencia de Pregrado
+                    </p>  
+                    <p>
+                        Gestión Institucional
+                    </p>
+                    <p>
+                        Vinculación con el Medio
+                    </p>
+                </div>
+                <!-- Grid column -->
+            
+        <!-- Grid row -->
+        </div>
+    </section>
+
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    <h6>© 2022 Iván Santis</h6>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
---
+++ templates/landing/vista-header/header.php
@@ -0,0 +1,36 @@
+<header class="header">
+    <div class="header-main">
+        <a class="a-logo" href="#">
+            <img class="logo" src="../public/assets/img/pizzarra.jpg" alt="logo-tipo" />
+        </a>
+        <a class="a-logo-duoc" href="#">
+            <img class="logo-imagen-duoc" src="../public/assets/img/duoc.jpg" alt="logo-tipo2" />
+        </a>
+
+        <div class="container-login d-none d-lg-flex">
+            <div class="button-admin">
+                <a href="/entradas/pendientes">
+                <img class="icon-admin" src="../public/assets/img/iconos_cafe/settings.png" alt="admin" />
+                </a>
+            </div>
+
+            <img class="logo-usuario" src="../public/assets/img/iconos_cafe/usuario.png" alt="logo-tipo" />
+            <img class="logo-buzon" src="../public/assets/img/iconos_cafe/email.png" alt="logo-tipo" />
+
+        </div>
+    </div>
+    
+</header>
+
+<nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="#">Home</a></li>
+    <li class="breadcrumb-item"><a href="#">Library</a></li>
+    <li class="breadcrumb-item active" aria-current="page">Data</li>
+  </ol>
+</nav>
+
+
+
+
+
